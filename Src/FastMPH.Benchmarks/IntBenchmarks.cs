3ad19fe0c528610d0acd42b2ca1729ccd1c23d1a
Ian Qvist <qvistian@gmail.com>
2026-04-11 17:20:30 +0200

    Add integer benchmarks

---
+++ Src/FastMPH.Benchmarks/IntBenchmarks.cs
@@ -0,0 +1,192 @@
+using System.Collections.Frozen;
+using System.Reflection;
+using BenchmarkDotNet.Order;
+using Genbox.FastMPH.BDZ;
+using Genbox.FastMPH.BBHash;
+using Genbox.FastMPH.BMZ;
+using Genbox.FastMPH.CHD;
+using Genbox.FastMPH.CHM;
+using Genbox.FastMPH.FCH;
+using Genbox.FastMPH.Internals;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Genbox.FastMPH.Benchmarks;
+
+[HideColumns("create", "search", "Gen0", "Gen1", "Gen2")]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[MemoryDiagnoser]
+public class IntBenchmarks
+{
+    private const int Capacity = 1_000_000;
+
+    public delegate object CFunc(ReadOnlySpan<int> data);
+
+    public delegate uint QFunc(int data);
+
+    private int[] _data = null!;
+    private int _query;
+
+    [GlobalSetup]
+    public void PrepareData()
+    {
+        // Unique integers from a deterministic RNG
+        HashSet<int> seen = new HashSet<int>(Capacity);
+        List<int> keys = new List<int>(Capacity);
+        while (keys.Count < Capacity)
+        {
+            int v = Random.Shared.Next();
+            if (seen.Add(v))
+                keys.Add(v);
+        }
+
+        _data = keys.ToArray();
+        _query = _data[_data.Length - 1];
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(GetConstructImpl))]
+    public object Construct(string name, double bitsPerItem, CFunc create) => create(_data);
+
+    [Benchmark]
+    [ArgumentsSource(nameof(GetQueryImpl))]
+    public uint Query(string name, double bitsPerItem, QFunc search) => search(_query);
+
+    /// <summary>
+    /// Convert a byte count to bits per item, rounded to 2 decimal places.
+    /// </summary>
+    private double BitsPerItem(uint stateBytes) => Math.Round(stateBytes * 8.0 / _data.Length, 2);
+
+    /// <summary>
+    /// Estimate Dictionary&lt;int, uint&gt; memory footprint in bytes.
+    /// </summary>
+    private static uint DictStateBytes(Dictionary<int, uint> dict)
+    {
+        int capacity = dict.EnsureCapacity(0);
+        return (uint)(112 + 20 * capacity);
+    }
+
+    /// <summary>
+    /// Estimate FrozenDictionary&lt;int, uint&gt; memory footprint in bytes via reflection.
+    /// </summary>
+    private static uint FrozenDictStateBytes(FrozenDictionary<int, uint> dict)
+    {
+        int n = dict.Count;
+        int b = GetFrozenHashTableBucketCount(dict);
+
+        if (b > 0)
+            return (uint)(144 + 8 * n + 8 * b);
+
+        return (uint)(96 + 8 * n);
+    }
+
+    private static int GetFrozenHashTableBucketCount(object frozen)
+    {
+        Type type = frozen.GetType();
+
+        FieldInfo? hashTableField = null;
+        for (Type? t = type; t != null && hashTableField == null; t = t.BaseType)
+            hashTableField = t.GetField("_hashTable", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        if (hashTableField != null)
+        {
+            object? hashTable = hashTableField.GetValue(frozen);
+            if (hashTable != null)
+            {
+                FieldInfo? bucketsField = hashTable.GetType().GetField("_buckets", BindingFlags.Instance | BindingFlags.NonPublic);
+                if (bucketsField != null && bucketsField.GetValue(hashTable) is Array buckets)
+                    return buckets.Length;
+            }
+        }
+
+        return 0;
+    }
+
+    public IEnumerable<object[]> GetConstructImpl()
+    {
+        PrepareData();
+
+        Dictionary<int, uint> dictPre = new Dictionary<int, uint>(_data.Length);
+        for (int i = 0; i < _data.Length; i++)
+            dictPre.Add(_data[i], (uint)i);
+        yield return
+        [
+            "Dict", BitsPerItem(DictStateBytes(dictPre)), new CFunc(data =>
+            {
+                Dictionary<int, uint> lookup = new Dictionary<int, uint>(data.Length);
+                for (int i = 0; i < data.Length; i++)
+                    lookup.Add(data[i], (uint)i);
+                return lookup;
+            })
+        ];
+
+        FrozenDictionary<int, uint> frozenPre = dictPre.ToFrozenDictionary();
+        yield return
+        [
+            "FrozenDict", BitsPerItem(FrozenDictStateBytes(frozenPre)), new CFunc(data =>
+            {
+                Dictionary<int, uint> tmp = new Dictionary<int, uint>(data.Length);
+                for (int i = 0; i < data.Length; i++)
+                    tmp.Add(data[i], (uint)i);
+                return tmp.ToFrozenDictionary();
+            })
+        ];
+
+        Validator.RequireThat(new ChdBuilder<int>(NullLogger<ChdBuilder<int>>.Instance).TryCreateMinimal(_data, out ChdMinimalState<int>? chdmPre));
+        yield return ["CHD_M", BitsPerItem(chdmPre.GetPackedSize()), new CFunc(data => new ChdBuilder<int>(NullLogger<ChdBuilder<int>>.Instance).TryCreateMinimal(data, out _, new ChdMinimalSettings()))];
+
+        Validator.RequireThat(new ChdBuilder<int>(NullLogger<ChdBuilder<int>>.Instance).TryCreate(_data, out ChdState<int>? chdPre));
+        yield return ["CHD", BitsPerItem(chdPre.GetPackedSize()), new CFunc(data => new ChdBuilder<int>(NullLogger<ChdBuilder<int>>.Instance).TryCreate(data, out _, new ChdSettings()))];
+
+        Validator.RequireThat(new BdzBuilder<int>(NullLogger<BdzBuilder<int>>.Instance).TryCreate(_data, out BdzState<int>? bdzPre));
+        yield return ["BDZ", BitsPerItem(bdzPre.GetPackedSize()), new CFunc(data => new BdzBuilder<int>(NullLogger<BdzBuilder<int>>.Instance).TryCreate(data, out _, new BdzSettings()))];
+
+        Validator.RequireThat(new BdzBuilder<int>(NullLogger<BdzBuilder<int>>.Instance).TryCreateMinimal(_data, out BdzMinimalState<int>? bdzmPre));
+        yield return ["BDZ_M", BitsPerItem(bdzmPre.GetPackedSize()), new CFunc(data => new BdzBuilder<int>(NullLogger<BdzBuilder<int>>.Instance).TryCreateMinimal(data, out _, new BdzMinimalSettings()))];
+
+        Validator.RequireThat(new BmzBuilder<int>(NullLogger<BmzBuilder<int>>.Instance).TryCreateMinimal(_data, out BmzMinimalState<int>? bmzPre));
+        yield return ["BMZ_M", BitsPerItem(bmzPre.GetPackedSize()), new CFunc(data => new BmzBuilder<int>(NullLogger<BmzBuilder<int>>.Instance).TryCreateMinimal(data, out _, new BmzMinimalSettings()))];
+
+        Validator.RequireThat(new ChmBuilder<int>(NullLogger<ChmBuilder<int>>.Instance).TryCreateMinimal(_data, out ChmMinimalState<int>? chmPre));
+        yield return ["CHM_M", BitsPerItem(chmPre.GetPackedSize()), new CFunc(data => new ChmBuilder<int>(NullLogger<ChmBuilder<int>>.Instance).TryCreateMinimal(data, out _, new ChmMinimalSettings()))];
+
+        Validator.RequireThat(new FchBuilder<int>(NullLogger<FchBuilder<int>>.Instance).TryCreateMinimal(_data, out FchMinimalState<int>? fchPre));
+        yield return ["FCH_M", BitsPerItem(fchPre.GetPackedSize()), new CFunc(data => new FchBuilder<int>(NullLogger<FchBuilder<int>>.Instance).TryCreateMinimal(data, out _, new FchMinimalSettings()))];
+
+        Validator.RequireThat(new BbHashBuilder<int>(NullLogger<BbHashBuilder<int>>.Instance).TryCreateMinimal(_data, out BbHashMinimalState<int>? bbPre));
+        yield return ["BB_M", BitsPerItem(bbPre.GetPackedSize()), new CFunc(data => new BbHashBuilder<int>(NullLogger<BbHashBuilder<int>>.Instance).TryCreateMinimal(data, out _, new BbHashMinimalSettings()))];
+    }
+
+    public IEnumerable<object[]> GetQueryImpl()
+    {
+        PrepareData();
+
+        Dictionary<int, uint> lookup = new Dictionary<int, uint>(_data.Length);
+        for (uint i = 0; i < _data.Length; i++)
+            lookup.Add(_data[i], i);
+        yield return ["Dict", BitsPerItem(DictStateBytes(lookup)), new QFunc(data => lookup.GetValueOrDefault(data, 0u))];
+
+        FrozenDictionary<int, uint> frozenDict = lookup.ToFrozenDictionary();
+        yield return ["FrozenDict", BitsPerItem(FrozenDictStateBytes(frozenDict)), new QFunc(data => frozenDict.GetValueOrDefault(data, 0u))];
+
+        Validator.RequireThat(new ChdBuilder<int>(NullLogger<ChdBuilder<int>>.Instance).TryCreateMinimal(_data, out ChdMinimalState<int>? chdmState));
+        yield return ["CHD_M", BitsPerItem(chdmState.GetPackedSize()), new QFunc(data => chdmState.Search(data))];
+
+        Validator.RequireThat(new ChdBuilder<int>(NullLogger<ChdBuilder<int>>.Instance).TryCreate(_data, out ChdState<int>? chdState));
+        yield return ["CHD", BitsPerItem(chdState.GetPackedSize()), new QFunc(data => chdState.Search(data))];
+
+        Validator.RequireThat(new BdzBuilder<int>(NullLogger<BdzBuilder<int>>.Instance).TryCreate(_data, out BdzState<int>? bdzState));
+        yield return ["BDZ", BitsPerItem(bdzState.GetPackedSize()), new QFunc(data => bdzState.Search(data))];
+
+        Validator.RequireThat(new BdzBuilder<int>(NullLogger<BdzBuilder<int>>.Instance).TryCreateMinimal(_data, out BdzMinimalState<int>? bdzmState));
+        yield return ["BDZ_M", BitsPerItem(bdzmState.GetPackedSize()), new QFunc(data => bdzmState.Search(data))];
+
+        Validator.RequireThat(new BmzBuilder<int>(NullLogger<BmzBuilder<int>>.Instance).TryCreateMinimal(_data, out BmzMinimalState<int>? bmzState));
+        yield return ["BMZ_M", BitsPerItem(bmzState.GetPackedSize()), new QFunc(data => bmzState.Search(data))];
+
+        Validator.RequireThat(new ChmBuilder<int>(NullLogger<ChmBuilder<int>>.Instance).TryCreateMinimal(_data, out ChmMinimalState<int>? chmState));
+        yield return ["CHM_M", BitsPerItem(chmState.GetPackedSize()), new QFunc(data => chmState.Search(data))];
+
+        Validator.RequireThat(new FchBuilder<int>(NullLogger<FchBuilder<int>>.Instance).TryCreateMinimal(_data, out FchMinimalState<int>? fchState));
+        yield return ["FCH_M", BitsPerItem(fchState.GetPackedSize()), new QFunc(data => fchState.Search(data))];
+    }
+}
---
+++ Src/FastMPH.Benchmarks/StringBenchmarks.cs
@@ -0,0 +1,183 @@
+using System.Collections.Frozen;
+using System.IO.Hashing;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using BenchmarkDotNet.Order;
+using Genbox.FastMPH.BDZ;
+using Genbox.FastMPH.Benchmarks.Misc;
+using Genbox.FastMPH.BBHash;
+using Genbox.FastMPH.BMZ;
+using Genbox.FastMPH.CHD;
+using Genbox.FastMPH.CHM;
+using Genbox.FastMPH.FCH;
+using Genbox.FastMPH.Internals;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Genbox.FastMPH.Benchmarks;
+
+[HideColumns("create", "search", "Gen0", "Gen1", "Gen2")]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[MemoryDiagnoser]
+public class StringBenchmarks
+{
+    public delegate object CFunc(ReadOnlySpan<string> data);
+
+    public delegate uint QFunc(string data);
+
+    private string[] _data = null!;
+    private string _query = null!;
+
+    [GlobalSetup]
+    public void PrepareData()
+    {
+        _data = StringHelper.GetRandomStrings(10, 1_000).ToArray();
+        _query = _data[_data.Length - 1];
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(GetConstructImpl))]
+    public object Construct(string name, double bitsPerItem, CFunc create) => create(_data);
+
+    [Benchmark]
+    [ArgumentsSource(nameof(GetQueryImpl))]
+    public uint Query(string name, double bitsPerItem, QFunc search) => search(_query);
+
+    /// <summary>
+    /// Convert a byte count to bits per item, rounded to 2 decimal places.
+    /// </summary>
+    private double BitsPerItem(uint stateBytes) => Math.Round(stateBytes * 8.0 / _data.Length, 2);
+
+    /// <summary>
+    /// Estimate Dictionary&lt;string, uint&gt; memory footprint in bytes (excluding string objects).
+    /// </summary>
+    private static uint DictStateBytes(Dictionary<string, uint> dict)
+    {
+        int capacity = dict.EnsureCapacity(0);
+        return (uint)(112 + 28 * capacity);
+    }
+
+    /// <summary>
+    /// Estimate FrozenDictionary&lt;string, uint&gt; memory footprint in bytes (excluding string objects).
+    /// </summary>
+    private static uint FrozenDictStateBytes(FrozenDictionary<string, uint> dict)
+    {
+        int n = dict.Count;
+        int b = GetFrozenHashTableBucketCount(dict);
+
+        if (b > 0)
+            return (uint)(168 + 16 * n + 8 * b);
+
+        return (uint)(120 + 12 * n);
+    }
+
+    private static int GetFrozenHashTableBucketCount(object frozen)
+    {
+        Type type = frozen.GetType();
+
+        FieldInfo? hashTableField = null;
+        for (Type? t = type; t != null && hashTableField == null; t = t.BaseType)
+            hashTableField = t.GetField("_hashTable", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        if (hashTableField != null)
+        {
+            object? hashTable = hashTableField.GetValue(frozen);
+            if (hashTable != null)
+            {
+                FieldInfo? bucketsField = hashTable.GetType().GetField("_buckets", BindingFlags.Instance | BindingFlags.NonPublic);
+                if (bucketsField != null && bucketsField.GetValue(hashTable) is Array buckets)
+                    return buckets.Length;
+            }
+        }
+
+        return 0;
+    }
+
+    public IEnumerable<object[]> GetConstructImpl()
+    {
+        PrepareData();
+
+        Dictionary<string, uint> dictPre = new Dictionary<string, uint>(_data.Length);
+        for (int i = 0; i < _data.Length; i++)
+            dictPre.Add(_data[i], (uint)i);
+        yield return
+        [
+            "Dict", BitsPerItem(DictStateBytes(dictPre)), new CFunc(data =>
+            {
+                Dictionary<string, uint> lookup = new Dictionary<string, uint>(data.Length);
+                for (int i = 0; i < data.Length; i++)
+                    lookup.Add(data[i], (uint)i);
+                return lookup;
+            })
+        ];
+
+        FrozenDictionary<string, uint> frozenPre = dictPre.ToFrozenDictionary();
+        yield return
+        [
+            "FrozenDict", BitsPerItem(FrozenDictStateBytes(frozenPre)), new CFunc(data =>
+            {
+                Dictionary<string, uint> tmp = new Dictionary<string, uint>(data.Length);
+                for (int i = 0; i < data.Length; i++)
+                    tmp.Add(data[i], (uint)i);
+                return tmp.ToFrozenDictionary();
+            })
+        ];
+
+        Validator.RequireThat(new ChdBuilder<string>(NullLogger<ChdBuilder<string>>.Instance).TryCreateMinimal(_data, out ChdMinimalState<string>? chdmPre));
+        yield return ["CHD_M", BitsPerItem(chdmPre.GetPackedSize()), new CFunc(data => new ChdBuilder<string>(NullLogger<ChdBuilder<string>>.Instance).TryCreateMinimal(data, out _, new ChdMinimalSettings()))];
+
+        Validator.RequireThat(new ChdBuilder<string>(NullLogger<ChdBuilder<string>>.Instance).TryCreate(_data, out ChdState<string>? chdPre));
+        yield return ["CHD", BitsPerItem(chdPre.GetPackedSize()), new CFunc(data => new ChdBuilder<string>(NullLogger<ChdBuilder<string>>.Instance).TryCreate(data, out _, new ChdSettings()))];
+
+        Validator.RequireThat(new BdzBuilder<string>(NullLogger<BdzBuilder<string>>.Instance).TryCreate(_data, out BdzState<string>? bdzPre));
+        yield return ["BDZ", BitsPerItem(bdzPre.GetPackedSize()), new CFunc(data => new BdzBuilder<string>(NullLogger<BdzBuilder<string>>.Instance).TryCreate(data, out _, new BdzSettings()))];
+
+        Validator.RequireThat(new BdzBuilder<string>(NullLogger<BdzBuilder<string>>.Instance).TryCreateMinimal(_data, out BdzMinimalState<string>? bdzmPre));
+        yield return ["BDZ_M", BitsPerItem(bdzmPre.GetPackedSize()), new CFunc(data => new BdzBuilder<string>(NullLogger<BdzBuilder<string>>.Instance).TryCreateMinimal(data, out _, new BdzMinimalSettings()))];
+
+        Validator.RequireThat(new BmzBuilder<string>(NullLogger<BmzBuilder<string>>.Instance).TryCreateMinimal(_data, out BmzMinimalState<string>? bmzPre));
+        yield return ["BMZ_M", BitsPerItem(bmzPre.GetPackedSize()), new CFunc(data => new BmzBuilder<string>(NullLogger<BmzBuilder<string>>.Instance).TryCreateMinimal(data, out _, new BmzMinimalSettings()))];
+
+        Validator.RequireThat(new ChmBuilder<string>(NullLogger<ChmBuilder<string>>.Instance).TryCreateMinimal(_data, out ChmMinimalState<string>? chmPre));
+        yield return ["CHM_M", BitsPerItem(chmPre.GetPackedSize()), new CFunc(data => new ChmBuilder<string>(NullLogger<ChmBuilder<string>>.Instance).TryCreateMinimal(data, out _, new ChmMinimalSettings()))];
+
+        Validator.RequireThat(new FchBuilder<string>(NullLogger<FchBuilder<string>>.Instance).TryCreateMinimal(_data, out FchMinimalState<string>? fchPre));
+        yield return ["FCH_M", BitsPerItem(fchPre.GetPackedSize()), new CFunc(data => new FchBuilder<string>(NullLogger<FchBuilder<string>>.Instance).TryCreateMinimal(data, out _, new FchMinimalSettings()))];
+
+        Validator.RequireThat(new BbHashBuilder<string>(NullLogger<BbHashBuilder<string>>.Instance).TryCreateMinimal(_data, out BbHashMinimalState<string>? bbPre));
+        yield return ["BB_M", BitsPerItem(bbPre.GetPackedSize()), new CFunc(data => new BbHashBuilder<string>(NullLogger<BbHashBuilder<string>>.Instance).TryCreateMinimal(data, out _, new BbHashMinimalSettings()))];
+    }
+
+    public IEnumerable<object[]> GetQueryImpl()
+    {
+        PrepareData();
+
+        Dictionary<string, uint> lookup = new Dictionary<string, uint>(_data.Length);
+        for (uint i = 0; i < _data.Length; i++)
+            lookup.Add(_data[i], i);
+        yield return ["Dict", BitsPerItem(DictStateBytes(lookup)), new QFunc(data => lookup.GetValueOrDefault(data, 0u))];
+
+        FrozenDictionary<string, uint> frozenDict = lookup.ToFrozenDictionary();
+        yield return ["FrozenDict", BitsPerItem(FrozenDictStateBytes(frozenDict)), new QFunc(data => frozenDict.GetValueOrDefault(data, 0u))];
+
+        Validator.RequireThat(new ChdBuilder<string>(NullLogger<ChdBuilder<string>>.Instance).TryCreateMinimal(_data, out ChdMinimalState<string>? state6));
+        yield return ["CHD_M", BitsPerItem(state6.GetPackedSize()), new QFunc(data => state6.Search(data))];
+
+        Validator.RequireThat(new ChdBuilder<string>(NullLogger<ChdBuilder<string>>.Instance).TryCreate(_data, out ChdState<string>? state7));
+        yield return ["CHD", BitsPerItem(state7.GetPackedSize()), new QFunc(data => state7.Search(data))];
+
+        Validator.RequireThat(new BdzBuilder<string>(NullLogger<BdzBuilder<string>>.Instance).TryCreate(_data, out BdzState<string>? state1));
+        yield return ["BDZ", BitsPerItem(state1.GetPackedSize()), new QFunc(data => state1.Search(data))];
+
+        Validator.RequireThat(new BdzBuilder<string>(NullLogger<BdzBuilder<string>>.Instance).TryCreateMinimal(_data, out BdzMinimalState<string>? state2));
+        yield return ["BDZ_M", BitsPerItem(state2.GetPackedSize()), new QFunc(data => state2.Search(data))];
+
+        Validator.RequireThat(new BmzBuilder<string>(NullLogger<BmzBuilder<string>>.Instance).TryCreateMinimal(_data, out BmzMinimalState<string>? state3));
+        yield return ["BMZ_M", BitsPerItem(state3.GetPackedSize()), new QFunc(data => state3.Search(data))];
+
+        Validator.RequireThat(new ChmBuilder<string>(NullLogger<ChmBuilder<string>>.Instance).TryCreateMinimal(_data, out ChmMinimalState<string>? state4));
+        yield return ["CHM_M", BitsPerItem(state4.GetPackedSize()), new QFunc(data => state4.Search(data))];
+
+        Validator.RequireThat(new FchBuilder<string>(NullLogger<FchBuilder<string>>.Instance).TryCreateMinimal(_data, out FchMinimalState<string>? state5));
+        yield return ["FCH_M", BitsPerItem(state5.GetPackedSize()), new QFunc(data => state5.Search(data))];
+    }
+}
